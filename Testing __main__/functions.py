cead4e8ea13daa630d7552fdb72087463c5c1d09
nicolai-dolleris <nicolainielsen31@gmail.com>
2025-02-07 20:21:43 +0100

    Test __main__

---
+++ Testing __main__/functions.py
@@ -0,0 +1,12 @@
+def add(x,y):
+    return x+y
+
+def multiply(x,y):
+    return x*y
+
+x = 5
+y = 8
+
+if __name__ == "__main__":
+    print(f"Now we are adding {add(x,y)}")
+    print(f"Multiplying {x} with {y} = {multiply(x,y)}")
---
+++ Testing __main__/main.py
@@ -0,0 +1,8 @@
+import functions
+
+print("This is the main script")
+
+x = 7
+y = 10
+
+print(functions.add(7,10))
